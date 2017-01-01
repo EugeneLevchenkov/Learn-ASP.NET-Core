5bfe926b7f487cf481a29496e529497582eed335
Eugene Levchenkov <eugene.levchenkov@yandex.by>
2017-01-01 18:57:04 +0300

    Moved Program and Startup classes.

--- SocialNetwork/src/SocialNetwork/Infrastructure/Startup.cs
+++ SocialNetwork/src/SocialNetwork/Infrastructure/Startup.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace SocialNetwork
+namespace SocialNetwork.Infrastructure
 {
     public class Startup
     {
