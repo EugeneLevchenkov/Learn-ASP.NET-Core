8bfc3b74834e565efef32da3ced0fad72d75f2a1
Eugene Levchenkov <eugene.levchenkov@yandex.by>
2017-01-02 19:16:49 +0300

    Added business layer.

---
+++ SocialNetwork/src/SocialNetwork.Business.Contracts/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Win-Yagd")]
+[assembly: AssemblyProduct("SocialNetwork.Business.Contracts")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d823c548-d804-4e81-a1e8-383c20dfd8bb")]
